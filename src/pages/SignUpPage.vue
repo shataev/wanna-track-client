ff714fec4b4fcd062c350cefe0ae400b77e8d5b2
viktor-shataev-unbiased-co-uk <viktor.shataev@unbiased.co.uk>
2023-06-20 09:06:09 +0700

    fix: chrome autofill issue

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -7,11 +7,28 @@
     subtitle-description="Create your account to continue"
     subtitle="Hello!"
   >
-    <app-input type="text" name="name" class="mb-3" placeholder="Name"></app-input>
-    <app-input type="email" name="email" class="mb-3" placeholder="Email"></app-input>
-    <app-input type="text" name="password" class="mb-3" placeholder="Password"></app-input>
-    <app-input type="text" name="confirmPassword" placeholder="Confirm Password"></app-input>
-    <app-button class="mt-12">Sign Up</app-button>
+    <app-input type="text" name="name" v-model="name" class="mb-3" placeholder="Name"></app-input>
+    <app-input
+      type="email"
+      name="email"
+      v-model="email"
+      class="mb-3"
+      placeholder="Email"
+    ></app-input>
+    <app-input
+      type="text"
+      name="password"
+      v-model="password"
+      class="mb-3"
+      placeholder="Password"
+    ></app-input>
+    <app-input
+      type="text"
+      name="confirmPassword"
+      v-model="confirmPassword"
+      placeholder="Confirm Password"
+    ></app-input>
+    <app-button class="mt-12" @click="sendData">Sign Up</app-button>
   </auth-layout>
 </template>
 
@@ -19,10 +36,39 @@
 import AppButton from '@/components/AppButton.vue'
 import AppInput from '@/components/AppInput.vue'
 import AuthLayout from '@/components/AuthLayout.vue'
+import { axiosInstance } from '@/api/axios'
 
 export default {
   name: 'SignUpPage',
-  components: { AuthLayout, AppInput, AppButton }
+  data() {
+    return {
+      name: null,
+      email: null,
+      password: null,
+      confirmPassword: null
+    }
+  },
+  components: { AuthLayout, AppInput, AppButton },
+  methods: {
+    async sendData(event) {
+      event.preventDefault()
+      try {
+        const response = await axiosInstance.post('/api/auth/signup', {
+          username: this.name,
+          email: this.email,
+          password: this.password
+        })
+
+        const { username, id, email, accessToken } = response.data
+
+        console.log(username, id, email, accessToken)
+
+        //await this.$router.push('/cost')
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
 </script>
 
