efc450bf5dfd7425fd924d1a444d019260d2116d
viktor-shataev-unbiased-co-uk <viktor.shataev@unbiased.co.uk>
2024-09-06 12:28:35 +0700

    draft: remove icons

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
+//import '@mdi/font/css/materialdesignicons.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
