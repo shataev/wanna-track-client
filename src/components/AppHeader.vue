3d690c3378f58f0c7c0b5a7396a17b42fa57aa5e
vshataev <vityashataev@gmail.com>
2024-10-28 14:27:59 +0700

    feat: add InnerPageLayout.vue and AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import LogoIcon from '@/components/icons/LogoIcon.vue'
+
+const { title } = defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <header class="d-flex mb-6 align-center">
+    <v-btn
+      icon="mdi-chevron-left"
+      @click="$router.back()"
+      height="35"
+      flat=""
+      width="35"
+      size="x-large"
+      color="#FFDF64"
+      class="button-back pa-0 text-app-green-lighter"
+    ></v-btn>
+    <h1 class="flex-grow-1 text-center text-app-light title">{{ title }}</h1>
+    <LogoIcon></LogoIcon>
+  </header>
+</template>
+
+<style scoped>
+.title {
+  font-size: 30px;
+  font-weight: 400;
+  line-height: 35px;
+  letter-spacing: -0.1804px;
+}
+
+.button-back {
+  font-size: 23px;
+}
+</style>
---
+++ src/layouts/InnerPageLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+
+const { title } = defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <app-header :title="title" />
+  <slot></slot>
+</template>
+
+<style scoped></style>
